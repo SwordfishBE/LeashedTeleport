a3b88ab4308c78eb784991cbeca20ac42755d892
Jimmy A. <6824251+SwordfishBE@users.noreply.github.com>
2026-03-30 12:30:36 +0200

    v1.1.3
    - Added a Modrinth update check..

--- src/main/java/net/leashedteleport/LeashedTeleportMod.java
+++ src/main/java/net/leashedteleport/LeashedTeleportMod.java
@@ -1,11 +1,13 @@
 package net.leashedteleport;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.leashedteleport.command.LeashedTeleportCommand;
 import net.leashedteleport.config.LeashedTeleportConfig;
 import net.leashedteleport.handler.LeashTeleportHandler;
 import net.leashedteleport.permission.PermissionManager;
+import net.leashedteleport.util.ModrinthUpdateChecker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +26,7 @@ public class LeashedTeleportMod implements ModInitializer {
         PermissionManager.refreshState();
         LeashedTeleportCommand.register();
         LeashTeleportHandler.registerEvents();
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> ModrinthUpdateChecker.checkOnceAsync());
         LOGGER.info("[LeashedTeleport] Mod initialized. Version: {}", VERSION);
     }
 }
